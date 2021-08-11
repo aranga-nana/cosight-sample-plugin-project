a3ed920b6bb176642bded39e6d88fe481f46efae
arangananayakkara <arangananayakkara@fintrix.com.au>
2021-08-11 18:05:54 +1000

    update libs

--- src/main/java/au/com/cosight/sample/PluginApp.java
+++ src/main/java/au/com/cosight/sample/PluginApp.java
@@ -5,6 +5,11 @@ import au.com.cosight.sdk.auth.external.oauth.ExternalOAuth2Credentials;
 import au.com.cosight.sdk.auth.external.oauth.ExternalOauth2Token;
 import au.com.cosight.sdk.plugin.runtime.CosightExecutionContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,12 +44,23 @@ public class PluginApp implements CommandLineRunner {
 
         final ExternalOAuth2Credentials credentials = ExternalOAuth2Credentials.getInstance();
         if (credentials.isValid()) {
-            logger.info("CLIENT ID{}",credentials.getOauth2Config().getClientId());
+            logger.info("CLIENT ID{}", credentials.getOauth2Config().getClientId());
             ExternalOauth2Token token = credentials.getToken();
-            logger.info("ACCESS TOKEN {}",token.getAccessToken());
+            logger.info("ACCESS TOKEN {}", token.getAccessToken());
+
+            try {
+
+
+                CloseableHttpClient httpClient = HttpClients.createDefault();
+                HttpGet httpGet = new HttpGet("https://api.xero.com/connections");
+                httpGet.setHeader("Authorization", "Bearer " + token.getAccessToken());
+                CloseableHttpResponse response = httpClient.execute(httpGet);
+                logger.info("{}",  EntityUtils.toString(response.getEntity()));
+
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
         }
-        logger.info("RUNTIME INFO{}",mapper.writeValueAsString(executionContext.getRuntimeInfo()));
-        logger.info("EXECUTION DONE....");
 
     }
 }
